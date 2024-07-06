33b4249871230f193de6c2df52a82a23e67a19d4
Rafeeq Muhammad <rafeeqnmuhammad@gmail.com>
2024-07-06 16:04:59 -0700

    fish aliases

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+# Aliases
+alias what="pwd && ls"
+alias configfish="vim ~/.config/fish/config.fish"
+alias sourcefish="source ~/.config/fish/config.fish"
+
+# Navigation Aliases
+alias .="cd .."
+alias ..="cd ../.."
+alias ...="cd ../../.."
+alias ....="cd ../../../.."
+alias .....="cd ../../../../.."
+alias ......="cd ../../../../../.."
