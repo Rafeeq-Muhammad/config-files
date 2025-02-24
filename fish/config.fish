777c28554da395f94ec8d1599729695c9855f0ac
Rafeeq Muhammad <rafeeqnmuhammad@gmail.com>
2025-02-23 17:02:59 -0800

    init

---
+++ fish/config.fish
@@ -0,0 +1,25 @@
+# Aliases
+alias what="pwd && ls"
+alias configfish="vim ~/.config/fish/config.fish"
+alias sourcefish="source ~/.config/fish/config.fish"
+
+# Navigation Aliases
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+
+function replace_brackets
+    # Join all arguments into a single string
+    set input (string join " " $argv)
+
+    # Replace [ with { and ] with }
+    set output (string replace -a "[" "{" -- $input)
+    set output (string replace -a "]" "}" -- $output)
+
+    # Print the result
+    echo $output
+end
+
+export GDK_BACKEND=x11
