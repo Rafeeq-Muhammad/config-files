e874123010dcfa16681be336407353b8729d9599
Rafeeq Muhammad <rafeeqnmuhammad@gmail.com>
2025-03-30 19:36:28 -0700

    gruvbox terminal theme

--- clone_config.sh
+++ clone_config.sh
@@ -1,3 +1,9 @@
+# fish theme gruvbox
+# curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+# fisher install Jomik/fish-gruvbox
+# theme_gruvbox dark soft
+
+
 git clone git@github.com:Rafeeq-Muhammad/config-files.git ~/config-temp
 mv ~/config-temp/* ~/config-temp/.* ~/.config 2>/dev/null
 rm -rf ~/config-temp
