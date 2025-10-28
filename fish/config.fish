4e9a36d91eb2525a27fca7411a28e92a6479e39d
Rafeeq Muhammad <RafeeqNMuhammad@gmail.com>
2025-10-27 22:57:41 -0700

    remove replace brackets

--- fish/config.fish
+++ fish/config.fish
@@ -49,18 +49,6 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias ......="cd ../../../../.."
 
-function replace_brackets
-    # Join all arguments into a single string
-    set input (string join " " $argv)
-
-    # Replace [ with { and ] with }
-    set output (string replace -a "[" "{" -- $input)
-    set output (string replace -a "]" "}" -- $output)
-
-    # Print the result
-    echo $output
-end
-
 function run
     if test (count $argv) -eq 0
         echo "Usage: run <file.(cpp|c|py)> [args...]"
